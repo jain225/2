1397326038ea9ca442548d19adcc511036d9cda9
jain225 <88730815+jain225@users.noreply.github.com>
2021-09-21 08:56:15 +0530

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,4 @@
+consumer_key        = 'voNBGYGtCpMXpDkbEZRhy8dRo'
+consumer_secret     = 'B10eGzfY5SjcfuYpxuAQcyAfsCJ9D3UQ22w710z9K8ScojBG7I'
+access_token        = '566329942-uMxumThnNIF1GgkaZxHBKEdSFkTJBvwK9jKaTcmm'
+access_token_secret = 'sEf4YtfOZfPkG8TaKLy6LJOvjUirIM8fY4VL1V6sC2qAm'
